0484c0eb8e8df161ea09ca82d4e387c51307b109
Monster2333333 <1311435497@qq.com>
2021-05-06 18:15:55 +0800

    trainer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -19,15 +19,16 @@ class Trainer(object):
 
     def __init__(self,
                  model: nn.Module,
+                 optimizer,
                  train_dataloader,
                  test_dataloader,
                  log_dir,
                  epochs,
                  log_per_epoch=10,
                  use_gpu=False,
-                 device=torch.device('cuda'),
-                 max_batch=None):
+                 device=torch.device('cuda')):
         self.model = model
+        self.optimizer = optimizer
         self.train_loader = train_dataloader
         self.test_loader = test_dataloader
         self.log_dir = log_dir
@@ -35,22 +36,16 @@ class Trainer(object):
         self.log_per_epoch = log_per_epoch
         self.use_gpu = use_gpu
         self.device = device
-        self.max_batch = max_batch
 
     def __eval(self):
         if self.use_gpu:
             self.model.to(self.device)
 
         for i, data in enumerate(self.test_loader):
-            if self.max_batch is not None:
-                if i >= self.max_batch:
-                    break
-
-            data = data["img"]
+            data = data[0]
             if self.use_gpu:
                 data = data.to(self.device)
-            self.model.set_input(data)
-            print("|eval loss: %.6f" % self.model.evaluate())
+            print("|eval loss: %.6f" % self.model.evaluate(data))
 
     def train(self):
         if self.use_gpu:
@@ -58,15 +53,16 @@ class Trainer(object):
 
         for epoch in range(1, self.epochs + 1):
             for i, data in enumerate(self.train_loader):
-                if self.max_batch is not None:
-                    if i >= self.max_batch:
-                        break
-                data = data["img"]
+                data = data[0]
                 if self.use_gpu:
                     data = data.to(self.device)
-                self.model.set_input(data)
-                self.model.optimize()
-                self.model.print_loss(epoch)
+                self.model(data)
+                loss = self.model.calc_loss()
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                print("|epoch: %d|loss: %.6f|" % (epoch, loss))
             self.__eval()
             if epoch % self.log_per_epoch == 0 and self.log_dir is not None:
                 path = os.path.join(self.log_dir, 'epoch_%d_vqvae_weight.pth' % epoch)
